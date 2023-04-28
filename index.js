035a93b64325f3833f67a4b9112579c81dbc2605
Kosiso60 <obinwakosiso@gmail.com>
2023-04-28 08:35:14 +0100

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express")
+require('dotenv').config();
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const app = express()
-
+app.use(cors());
 
 app.use(bodyParser.json())
 
 PORT = 5000
-const MONGO_URL = "mongodb+srv://emibrandlance:emibrandlance@cluster0.jcqf7hv.mongodb.net/?retryWrites=true&w=majority"
 
-
-
-mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true, 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT , ()=>{
         console.log(`listening on port ${PORT}`)
@@ -22,3 +22,17 @@ mongoose.connect(MONGO_URL, {
 }).catch((err) => {
     console.log(err.message + "Errorrrrrrr")
 })
+app.get('/', (req, res) => {
+    res.render('Hello World!');
+  });
+app.get('/signup', (req, res) => {
+    res.render('register');
+  })
+app.post('/login', (req, res) => {
+    res.render('login');
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
